26bc821b3c0ad9ff5d3aff1686880146a5c7eb1a
VELLANKI NITISH BALA SOURI RAJ <nitishvellanki@outlook.com>
2026-05-26 14:20:45 +0530

    Add: two-integer-sum - submission-2

---
+++ Data Structures & Algorithms/two-integer-sum/submission-2.py
@@ -0,0 +1,9 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+       c={}
+       for i,val in enumerate(nums):
+        y=target-val
+        if y in c:
+            return [c[y],i]
+        c[val]=i
+        
